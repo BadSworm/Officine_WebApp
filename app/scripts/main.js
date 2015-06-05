c15dc3f241c957914d4f9789f4b6dc8286775a2a
BadSworm@Rebus <badsworm@alice.it>
2015-06-05 15:48:18 +0200

    Preweekend push

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,1 @@
-//console.log('\'Allo \'Allo!');
-$(document).ready(function () {
-  $('.slide-show').slick({
-    dots: true,
-    speed: 500,
-    autoplay: true,
-    arrows: false,
-    fade: true
-  });
-});
+console.log('\'Allo \'Allo!');
